c2093dc4dd7aec7c9709debbdde9a3ff8f162439
Mingshen Sun <bob@mssun.me>
2018-08-13 23:58:04 -0700

    Start collecting compiler feature status

--- ci/script.sh
+++ ci/script.sh
@@ -2,6 +2,13 @@
 
 pushd rust
 
+# compiler features
+rg ".*?\((active|removed|accepted),\s*(.*?),\s*\"(.*?)\",\s*(.*?),\s*(.*?)\)," -r '$1:$2:$3:$4:$5' -g '!src/test/*' --no-heading --line-number \
+    | cut -d ':' -f1,2,3,4,5,6,7 --output-delimiter=' ' \
+    | awk -v ORS='' '{print ""$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"; system("git --no-pager blame -L "$2","$2" -c  -- "$1" | cut -f3")}' \
+    | uniq \
+    | sort -k1,1 > compiler_feature_raw.txt
+
 # stable library features or features will be stabilized in next few releases
 rg ".*#\[stable\(feature\s*=\s*\"(.*?)\",.*since\s*=\s*\"(.*?)\"\)[,\]].*" -r '$1:$2' -g '!src/test/*' --no-heading --line-number \
     | cut -d ':' -f1,2,3,4 --output-delimiter=' ' \
