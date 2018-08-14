292ff93b2b9bb1119d1df418d73eca69f5c1917e
Mingshen Sun <bob@mssun.me>
2018-08-14 11:22:53 -0700

    Use '\t' as delimiter for awk

--- ci/script.sh
+++ ci/script.sh
@@ -5,14 +5,14 @@ pushd rust
 # compiler features
 rg ".*?\((active|removed|accepted),\s*(.*?),\s*\"(.*?)\",\s*(.*?),\s*(.*?)\)," -r '$1:$2:$3:$4:$5' -g '!src/test/*' --no-heading --line-number \
     | sed -E 's/:/\n/g7; s/:/\t/g; s/\n/:/g' \
-    | awk -v ORS='' '{print ""$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"; system("git --no-pager blame -L "$2","$2" -c  -- "$1" | cut -f3")}' \
+    | awk -v ORS='' -F '\t' '{print ""$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"; system("git --no-pager blame -L "$2","$2" -c  -- "$1" | cut -f3")}' \
     | uniq \
     | sort -k1,1 > compiler_feature_raw.txt
 
 # stable library features or features will be stabilized in next few releases
 rg ".*#\[stable\(feature\s*=\s*\"(.*?)\",.*since\s*=\s*\"(.*?)\"\)[,\]].*" -r '$1:$2' -g '!src/test/*' --no-heading --line-number \
     | cut -d ':' -f1,2,3,4 --output-delimiter='\t' \
-    | awk -v ORS='' '{print ""$3"\t"$4"\t"; system("git --no-pager blame -L "$2","$2" -c  -- "$1" | cut -f3")}' \
+    | awk -v ORS='' -F '\t' '{print ""$3"\t"$4"\t"; system("git --no-pager blame -L "$2","$2" -c  -- "$1" | cut -f3")}' \
     | uniq > stable_library_feature.txt
 
 cat stable_library_feature.txt \
@@ -26,7 +26,7 @@ cat stable_library_feature.txt \
 # unstable library features which do not have ETA
 rg ".*#\[unstable\(feature\s*=\s*\"(.*?)\",.*issue\s*=\s*\"(.*?)\"[,\)].*" -r '$1:$2' -g '!src/test/*' --no-heading --line-number \
     | cut -d ':' -f1,2,3,4 --output-delimiter='\t' \
-    | awk -v ORS='' '{print ""$3"\t"$4"\t"; system("git --no-pager blame -L "$2","$2" -c  -- "$1" | cut -f3")}' \
+    | awk -v ORS='' -F '\t' '{print ""$3"\t"$4"\t"; system("git --no-pager blame -L "$2","$2" -c  -- "$1" | cut -f3")}' \
     | uniq > unstable_library_feature.txt
 
 cat unstable_library_feature.txt \
