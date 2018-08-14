2ced9f8f8e11075090b8abeff21d5f49d3e53bb2
Mingshen Sun <bob@mssun.me>
2018-08-14 11:08:48 -0700

    Use sed instead of cut to get first k occurences

--- ci/script.sh
+++ ci/script.sh
@@ -4,14 +4,14 @@ pushd rust
 
 # compiler features
 rg ".*?\((active|removed|accepted),\s*(.*?),\s*\"(.*?)\",\s*(.*?),\s*(.*?)\)," -r '$1:$2:$3:$4:$5' -g '!src/test/*' --no-heading --line-number \
-    | cut -d ':' -f1,2,3,4,5,6,7 --output-delimiter=' ' \
+    | sed -E 's/:/\n/g7; s/:/\t/g; s/\n/:/g' \
     | awk -v ORS='' '{print ""$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"; system("git --no-pager blame -L "$2","$2" -c  -- "$1" | cut -f3")}' \
     | uniq \
     | sort -k1,1 > compiler_feature_raw.txt
 
 # stable library features or features will be stabilized in next few releases
 rg ".*#\[stable\(feature\s*=\s*\"(.*?)\",.*since\s*=\s*\"(.*?)\"\)[,\]].*" -r '$1:$2' -g '!src/test/*' --no-heading --line-number \
-    | cut -d ':' -f1,2,3,4 --output-delimiter=' ' \
+    | cut -d ':' -f1,2,3,4 --output-delimiter='\t' \
     | awk -v ORS='' '{print ""$3"\t"$4"\t"; system("git --no-pager blame -L "$2","$2" -c  -- "$1" | cut -f3")}' \
     | uniq > stable_library_feature.txt
 
@@ -25,7 +25,7 @@ cat stable_library_feature.txt \
 
 # unstable library features which do not have ETA
 rg ".*#\[unstable\(feature\s*=\s*\"(.*?)\",.*issue\s*=\s*\"(.*?)\"[,\)].*" -r '$1:$2' -g '!src/test/*' --no-heading --line-number \
-    | cut -d ':' -f1,2,3,4 --output-delimiter=' ' \
+    | cut -d ':' -f1,2,3,4 --output-delimiter='\t' \
     | awk -v ORS='' '{print ""$3"\t"$4"\t"; system("git --no-pager blame -L "$2","$2" -c  -- "$1" | cut -f3")}' \
     | uniq > unstable_library_feature.txt
 
